52bfe1147b80beb4ce28a1d47f2d3724c0589ff5
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-07 17:05:43 +0900

    feat: enhance package export and artifact paths handling

--- Assets/Editor/PackageExporter.cs
+++ Assets/Editor/PackageExporter.cs
@@ -19,8 +19,7 @@ public sealed class PackageExporter
     public static void Export()
     {
         var options = ArgumentsParser.GetValidatedOptions();
-        var buildPath = options.GetValueOrDefault("buildsPath").TrimEnd('/');
-        var exportPath = "./" + (!string.IsNullOrEmpty(buildPath) ? buildPath + "/" : "") + ToExportPath(_exportPath, options.GetValueOrDefault("tag"));
+        var exportPath = "./" + ToExportPath(_exportPath, options.GetValueOrDefault("tag"));
         Export(_folderPath, exportPath);
     }
 
