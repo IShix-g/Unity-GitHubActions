3ef8df18ef7d9614aa497b52f18a603f589d3416
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-11 12:30:56 +0900

    refactor: adjust using directives for consistency

--- Assets/Plugins/PackageExporter/Editor/Builder.cs
+++ Assets/Plugins/PackageExporter/Editor/Builder.cs
@@ -3,8 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using UnityEditor;
 using UnityEngine;
+using UnityEditor;
 
 namespace PackageExporter.Editor
 {
