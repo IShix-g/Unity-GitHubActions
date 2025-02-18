5368be6d9ce49d6272c98205baca419fba8feb66
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-18 13:20:22 +0900

    fix code indentation in StyleTestScript.cs

--- Assets/Scripts/StyleTestScript.cs
+++ Assets/Scripts/StyleTestScript.cs
@@ -34,14 +34,13 @@ namespace Tests
 
         public double GetDouble() => 10;
 
-        public void ExampleMethod()
-        {
-            // 未使用の変数で警告
-            int unusedVariable;
+    public void ExampleMethod()
+    {
+        // 未使用の変数で警告
+        int unusedVariable;
 
-            // 意図的にコンパイルエラー
-            not_defined_variable = 10;
-        }
-
+        // 意図的にコンパイルエラー
+        not_defined_variable = 10;
+    }
     }
 }
