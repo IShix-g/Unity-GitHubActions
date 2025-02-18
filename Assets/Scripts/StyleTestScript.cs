1c16e699b237ac51b1f3071a0743755ae4a3233f
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-18 13:42:55 +0900

    remove unused method and problematic code

--- Assets/Scripts/StyleTestScript.cs
+++ Assets/Scripts/StyleTestScript.cs
@@ -35,14 +35,5 @@ namespace Tests
         public float GetFloat() => 10;
 
         public double GetDouble() => 10;
-
-        public void ExampleMethod()
-        {
-            // 未使用の変数で警告
-            int unusedVariable;
-
-            // 意図的にコンパイルエラー
-            not_defined_variable = 10;
-        }
     }
 }
