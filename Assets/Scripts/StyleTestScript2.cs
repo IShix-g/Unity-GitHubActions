5af68e431753f65311b572e34ce15fedba517048
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-21 11:33:13 +0900

    feat: add StyleTestScript2 for coding style demonstrations

---
+++ Assets/Scripts/StyleTestScript2.cs
@@ -0,0 +1,41 @@
+
+using UnityEngine;
+
+namespace Tests
+{
+    public class RuleChecker
+    {
+        private int _incorrect_field;
+        private string stringField;
+     int _intField;
+        public int CorrectProperty { get; set; }
+
+        public void CheckRules()
+        {
+            // 条件ブロックにブレースがないのでルール違反 (SA1503)
+            if (CorrectProperty > 0)
+                Debug.Log("Correct Property is greater than zero");
+
+            // 正しい例：必ずブレースを使用する
+            if (CorrectProperty > 0)
+            {
+                Debug.Log("Correct Property is greater than zero");
+            }
+        }
+
+        public void VariableDeclaration()
+        {
+            var number = 5;
+            int specificNumber = 10;
+        }
+    }
+
+    class improperly_named_class
+    {
+        public void ImproperMethod()
+        {
+
+        }
+    }
+
+}
