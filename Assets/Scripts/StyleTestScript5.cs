dfca7b0e25ef12cc9da7687034769f64348931de
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-22 10:28:58 +0900

    refactor: reorder and clean up class properties and fields

--- Assets/Scripts/StyleTestScript5.cs
+++ Assets/Scripts/StyleTestScript5.cs
@@ -3,12 +3,11 @@ namespace Tests
 {
     public sealed class StyleTestScript5
     {
+        public int CorrectProperty { get; set; }
         private int _incorrectField;
         private string stringField;
 
-        public int CorrectProperty { get; set; }
-
-        public static readonly string StaticProperty = "This is a static property";
+       public static readonly string StaticProperty = "This is a static property";
         static readonly string staticField = "This is a static field";
         static string staticField2 = "This is a static field";
 
