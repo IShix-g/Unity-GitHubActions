1187bda495bbcc7cd76fb00cd4183ab5eb65c3cc
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-22 20:22:57 +0900

    feat: add StyleTestScript2 for testing code style rules

---
+++ Assets/Tests/StyleTest/StyleTestScript2.cs
@@ -0,0 +1,24 @@
+namespace Tests.StyleTest
+{
+    public sealed class StyleTestScript2
+    {
+        private int _incorrectField;
+        private string stringField;
+
+        public int CorrectProperty { get; set; }
+
+        public static readonly string StaticProperty = "This is a static property";
+        static readonly string staticField = "This is a static field";
+        static string staticField2 = "This is a static field";
+
+        public void CheckRules()
+        {
+
+        }
+
+        public void variableDeclaration()
+        {
+
+        }
+    }
+}
