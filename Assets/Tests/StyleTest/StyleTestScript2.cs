5d4aa64991e133cdc295133c3f3fbfb27d0fa2be
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-23 19:17:30 +0900

    refactor: fix code formatting in StyleTestScript2 functionality was

--- Assets/Tests/StyleTest/StyleTestScript2.cs
+++ Assets/Tests/StyleTest/StyleTestScript2.cs
@@ -3,9 +3,9 @@ namespace Tests.StyleTest
     public sealed class StyleTestScript2
     {
         private int _incorrectField;
-        private string stringField;
+         private string stringField;
 
-            public int CorrectProperty { get; set; }
+        public int CorrectProperty { get; set; }
 
         public static readonly string StaticProperty = "This is a static property";
         static readonly string staticField = "This is a static field";
