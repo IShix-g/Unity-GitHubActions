791e11d229be1e90023fdfd052885d163dbf8d2e
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-25 14:30:05 +0900

    add TestConst constant to StyleTest class

--- Assets/Tests/StyleTest/StyleTest.cs
+++ Assets/Tests/StyleTest/StyleTest.cs
@@ -3,7 +3,9 @@ namespace Tests.StyleTest
 {
     public sealed class StyleTest
     {
+        public const string TestConst = "Test";
         static string s_staticField = string.Empty;
+
         string _test;
         string _test2;
 
