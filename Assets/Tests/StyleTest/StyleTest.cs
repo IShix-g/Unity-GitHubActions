6603521bafaca85cc02c518b11cc337ba17c6d20
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-25 14:08:42 +0900

    feat: add StyleTest class for future style validation

---
+++ Assets/Tests/StyleTest/StyleTest.cs
@@ -0,0 +1,15 @@
+
+namespace Tests.StyleTest
+{
+    public sealed class StyleTest
+    {
+        static string staticField = "";
+        string test;
+        string _test2;
+
+         void Test()
+        {
+
+        }
+    }
+}
