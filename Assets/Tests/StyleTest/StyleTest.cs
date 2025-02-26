2259f2110ff660bbe41a4fae62f2f3e6764802b7
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-26 11:20:55 +0900

    refactor: fix inconsistent formatting in StyleTest.cs

--- Assets/Tests/StyleTest/StyleTest.cs
+++ Assets/Tests/StyleTest/StyleTest.cs
@@ -3,16 +3,16 @@ namespace Tests.StyleTest
 {
     public sealed class StyleTest
     {
-       public const string TestConst = "Test";
+        public const string TestConst = "Test";
         public static readonly string TestStaticConst = "Test";
-        static string s_staticField = string.Empty;
+   static string s_staticField = string.Empty;
 
         string _test;
         string _test2;
-    int _test3;
+        int _test3;
         double _test4;
 
-        void Test()
+       void Test()
         {
 
        }
