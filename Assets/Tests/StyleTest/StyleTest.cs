768223c59659179d5f87ab9547fbe96727b46382
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-26 11:23:07 +0900

    refactor: fix inconsistent code indentation

--- Assets/Tests/StyleTest/StyleTest.cs
+++ Assets/Tests/StyleTest/StyleTest.cs
@@ -5,16 +5,16 @@ namespace Tests.StyleTest
     {
         public const string TestConst = "Test";
         public static readonly string TestStaticConst = "Test";
-   static string s_staticField = string.Empty;
+        static string s_staticField = string.Empty;
 
-        string _test;
+       string _test;
         string _test2;
         int _test3;
         double _test4;
 
        void Test()
-        {
+       {
 
-       }
+   }
     }
 }
