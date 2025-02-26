4dcff10b35a53d3568b687d871c5f9d46509393c
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-26 11:17:37 +0900

    refactor: fix inconsistent code indentation

--- Assets/Tests/StyleTest/StyleTest.cs
+++ Assets/Tests/StyleTest/StyleTest.cs
@@ -3,13 +3,13 @@ namespace Tests.StyleTest
 {
     public sealed class StyleTest
     {
-        public const string TestConst = "Test";
+       public const string TestConst = "Test";
         public static readonly string TestStaticConst = "Test";
-    static string s_staticField = string.Empty;
+        static string s_staticField = string.Empty;
 
         string _test;
         string _test2;
-        int _test3;
+    int _test3;
         double _test4;
 
         void Test()
