b25274c269437b2904cf0543e0cae5e3e079dcad
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-28 15:46:42 +0900

    feat: add StyleTest3 class for style testing

---
+++ Assets/Tests/StyleTest/StyleTest3.cs
@@ -0,0 +1,14 @@
+namespace Tests.StyleTest
+{
+    public sealed class StyleTest3
+    {
+        public static readonly string TestStaticReadonly = "TestStaticReadonly";
+        static string _staticField = string.Empty;
+        string _test;
+        string test2;
+
+        void Test()
+        {
+       }
+    }
+}
