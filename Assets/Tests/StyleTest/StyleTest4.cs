974ff395862888f1cf53157593eb2c6c9c8d568d
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-28 18:03:25 +0900

    feat: add new StyleTest4 class for testing styles

---
+++ Assets/Tests/StyleTest/StyleTest4.cs
@@ -0,0 +1,15 @@
+namespace Tests.StyleTest
+{
+    public sealed class StyleTest4
+    {
+        static string _staticField = string.Empty;
+        string _test;
+        string _test2;
+
+        void Test()
+        {
+            var test = this._test2;
+
+        }
+    }
+}
