a937c33c13ad8f7dcdb6abb250e0ffcc911f6fd3
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-02-28 15:50:08 +0900

    refactor: rename static field for consistency

--- Assets/Tests/StyleTest/StyleTest3.cs
+++ Assets/Tests/StyleTest/StyleTest3.cs
@@ -3,12 +3,12 @@ namespace Tests.StyleTest
     public sealed class StyleTest3
     {
         public static readonly string TestStaticReadonly = "TestStaticReadonly";
-        static string _staticField = string.Empty;
+        static string s_staticField = string.Empty;
         string _test;
         string test2;
 
         void Test()
         {
-       }
+        }
     }
 }
