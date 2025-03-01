9ffeac4fd13adbcafbdae6511c0f376b4cb0cb9e
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-01 12:13:52 +0900

    refactor: fix code styling issues in StyleTest classes

--- Assets/Tests/StyleTest/StyleTest3.cs
+++ Assets/Tests/StyleTest/StyleTest3.cs
@@ -5,7 +5,7 @@ namespace Tests.StyleTest
         public static readonly string TestStaticReadonly = "TestStaticReadonly";
         static string s_staticField = string.Empty;
         string _test;
-        string _test2;
+        string test2;
 
         void Test()
         {
