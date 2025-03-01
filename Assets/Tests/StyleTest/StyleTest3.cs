2217e2b200e00f32d6689e356a68fb5a8bdfebf6
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-01 12:26:20 +0900

    refactor: fix code style inconsistencies in test files

--- Assets/Tests/StyleTest/StyleTest3.cs
+++ Assets/Tests/StyleTest/StyleTest3.cs
@@ -5,7 +5,7 @@ namespace Tests.StyleTest
         public static readonly string TestStaticReadonly = "TestStaticReadonly";
         static string s_staticField = string.Empty;
         string _test;
-        string test2;
+        string _test2;
 
         void Test()
         {
