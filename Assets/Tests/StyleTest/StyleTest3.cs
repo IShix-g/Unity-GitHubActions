d6aab983b23629ff120ebf0c979ebc9b920868fb
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-05 15:32:47 +0900

    feat: add StyleTest3 class for testing code styles

---
+++ Assets/Tests/StyleTest/StyleTest3.cs
@@ -0,0 +1,17 @@
+
+namespace Tests.StyleTest
+{
+    public sealed class StyleTest3
+    {
+        public const string ConstString = "ConstString";
+        static string s_staticField = string.Empty;
+        string _stringField;
+        string _stringField2;
+        string _stringField3;
+        
+        void Method()
+        {
+            
+        }
+    }
+}
