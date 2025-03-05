9e793a1144eb48d22ba2cfa8a04d72021b791f78
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-05 16:04:58 +0900

    feat: add StyleTest4 class for testing coding styles

---
+++ Assets/Tests/StyleTest/StyleTest4.cs
@@ -0,0 +1,16 @@
+namespace Tests.StyleTest
+{
+    public sealed class StyleTest4
+    {
+        public const string ConstString = "ConstString";
+        static string s_staticField = string.Empty;
+        string _stringField;
+        string _stringField2;
+        string stringField3;
+        
+        void Method()
+        {
+            
+        }
+    }
+}
