94d25e691cc86505fa66364092bc8812bd038c91
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-05 17:17:01 +0900

    refactor: improve naming consistency in StyleTest classes

--- Assets/Tests/StyleTest/StyleTest5.cs
+++ Assets/Tests/StyleTest/StyleTest5.cs
@@ -1,16 +1,17 @@
+
 namespace Tests.StyleTest
-{
-    public sealed class StyleTest4
     {
+    public sealed class StyleTest5
+        {
         public const string ConstString = "ConstString";
         static string s_staticField = string.Empty;
         string _stringField;
         string _stringField2;
-        string _stringField3;
+        string stringField3;
         
         void Method()
-        {
+            {
             
+            }
         }
     }
-}
