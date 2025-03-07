003504b9f2d7742442c6130c11ee4816d06b717a
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-07 10:49:35 +0900

    refactor: remove unnecessary whitespace in StyleTest9.cs

--- Assets/Tests/StyleTest/StyleTest9.cs
+++ Assets/Tests/StyleTest/StyleTest9.cs
@@ -2,20 +2,18 @@ namespace Tests.StyleTest
 {
     public sealed class StyleTest9
     {
-       public const string ConstString = "ConstString";
+        public const string ConstString = "ConstString";
         static string s_staticField = string.Empty;
         string _stringField;
         string _stringField2;
         string _stringField3;
-        
+
         void Method()
         {
-            
         }
 
         void Method2()
         {
-            
         }
     }
 }
