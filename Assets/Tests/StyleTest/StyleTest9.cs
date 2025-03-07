4b9b65465c3ff829ee1e7a979bc3c7d0303e0c54
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-07 14:54:26 +0900

    refactor: improve code style for consistency

--- Assets/Tests/StyleTest/StyleTest9.cs
+++ Assets/Tests/StyleTest/StyleTest9.cs
@@ -2,18 +2,18 @@ namespace Tests.StyleTest
 {
     public sealed class StyleTest9
     {
-        public const string ConstString = "ConstString";
-       static string s_staticField = string.Empty;
+       public const string ConstString = "ConstString";
+        static string s_staticField = string.Empty;
         string _stringField;
         string _stringField2;
-        string stringField3;
+        string _stringField3;
         
-        void Method()
+        void _method()
         {
             
         }
 
-        void _Method2()
+        void Method2()
         {
         }
     }
