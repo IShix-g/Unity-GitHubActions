ab690bd6e0cd7927248e616606f51c5df77c76ab
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-07 14:50:59 +0900

    refactor: update StyleTest9 field and method naming conventions

--- Assets/Tests/StyleTest/StyleTest9.cs
+++ Assets/Tests/StyleTest/StyleTest9.cs
@@ -3,17 +3,17 @@ namespace Tests.StyleTest
     public sealed class StyleTest9
     {
         public const string ConstString = "ConstString";
-        static string s_staticField = string.Empty;
+       static string s_staticField = string.Empty;
         string _stringField;
         string _stringField2;
-        string _stringField3;
-
+        string stringField3;
+        
         void Method()
         {
             
         }
 
-        void Method2()
+        void _Method2()
         {
         }
     }
