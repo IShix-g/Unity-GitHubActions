9ce084fd643475fa09d9459b48a667e0842ddc0c
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-07 15:45:55 +0900

    refactor: rename StyleTest9 to StyleTest10

--- Assets/Tests/StyleTest/StyleTest10.cs
+++ Assets/Tests/StyleTest/StyleTest10.cs
@@ -1,6 +1,6 @@
 namespace Tests.StyleTest
 {
-    public sealed class StyleTest9
+    public sealed class StyleTest10
     {
         public const string ConstString = "ConstString";
         static string s_staticField = string.Empty;
