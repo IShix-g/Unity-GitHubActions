06a4ee7855e3e3db866335e99ffefba8ad47807d
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-07 10:47:00 +0900

    refactor: correct inconsistent indentation in StyleTest9.cs

--- Assets/Tests/StyleTest/StyleTest9.cs
+++ Assets/Tests/StyleTest/StyleTest9.cs
@@ -2,9 +2,9 @@ namespace Tests.StyleTest
 {
     public sealed class StyleTest9
     {
-        public const string ConstString = "ConstString";
+       public const string ConstString = "ConstString";
         static string s_staticField = string.Empty;
-       string _stringField;
+        string _stringField;
         string _stringField2;
         string _stringField3;
         
