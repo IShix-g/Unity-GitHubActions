49b8ae7dea6c033eaeb14e379bf331cefaff8b04
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-07 14:34:17 +0900

    refactor: rename field for consistency with coding standards

--- Assets/Tests/StyleTest/StyleTest9.cs
+++ Assets/Tests/StyleTest/StyleTest9.cs
@@ -6,7 +6,7 @@ namespace Tests.StyleTest
         static string s_staticField = string.Empty;
         string _stringField;
         string _stringField2;
-        string _stringField3;
+        string stringField3;
 
         void Method()
         {
