f67fdda81ac93d6a3e8c236d5b8de2ccad43ed11
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-10 10:06:00 +0900

    feat: add StyleTest12 for style validation

---
+++ Assets/Tests/StyleTest/StyleTest12.cs
@@ -0,0 +1,14 @@
+namespace Tests.StyleTest
+{
+    public sealed class StyleTest12
+    {
+        private int TestVariable = 10;
+
+        // 正しい名前のフィールド
+        private string _exampleField = "Valid";
+
+        void Test(){}
+
+      void Test2(){}
+    }
+}
