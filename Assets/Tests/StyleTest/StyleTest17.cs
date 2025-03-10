9469b271d20d5efb8355f70013c772f189a831a3
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-10 15:54:32 +0900

    refactor: update naming and logging in style tests

--- Assets/Tests/StyleTest/StyleTest17.cs
+++ Assets/Tests/StyleTest/StyleTest17.cs
@@ -1,7 +1,7 @@
 
 namespace Tests.StyleTest
 {
-    public sealed class StyleTest16
+    public sealed class StyleTest17
     {
       static string s_staticField = string.Empty;
         string _test;
