e0de37175a4a180f8ba1810b5f05f51319659fd6
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-10 15:24:54 +0900

    feat: add StyleTest12 with initial structure and config update

---
+++ Assets/Tests/StyleTest/StyleTest12.cs
@@ -0,0 +1,19 @@
+namespace Tests.StyleTest
+{
+    public sealed class StyleTest12
+    {
+      static string s_staticField = string.Empty;
+        string _test;
+        string _test2;
+        
+        void Test()
+        {
+
+        }
+
+        void _Test2()
+        {
+
+        }
+    }
+}
