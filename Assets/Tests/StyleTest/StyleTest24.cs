d1c60b1e1ce0876f5f0a533dc40a997f85e15e39
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-10 18:13:48 +0900

    refactor: rename and simplify style test handling

--- Assets/Tests/StyleTest/StyleTest24.cs
+++ Assets/Tests/StyleTest/StyleTest24.cs
@@ -1,7 +1,7 @@
 
 namespace Tests.StyleTest
 {
-    public sealed class StyleTest23
+    public sealed class StyleTest24
     {
       static string s_staticField = string.Empty;
         string _test;
