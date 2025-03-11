f20c85a63513f1005cce84a2afe4134ec75353b8
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-03-11 15:23:32 +0900

    refactor: update code style settings and rename test file

--- Assets/Tests/StyleTest/StyleTest25.cs
+++ Assets/Tests/StyleTest/StyleTest25.cs
@@ -1,16 +1,16 @@
-
 namespace Tests.StyleTest
 {
-    public sealed class StyleTest24
+    public sealed class StyleTest25
     {
-      static string s_staticField = string.Empty;
+        static string s_staticField = string.Empty;
         string _test;
         string _test2;
-        
+
         void Test()
         {
 
         }
+
 
         void _Test2()
         {
